b9f2cd7ed9f65bc6a964508701fbd5501ee78a99
Tphohlela <17thapho@gmail.com>
2025-02-21 14:12:59 +0200

    Trying to resolve alpine

--- public/notes-app.js
+++ public/notes-app.js
@@ -1,20 +1,19 @@
-export default function notesApp(){
-  return {
-    count: 0,
+export default function notesApp() {
+    return {
+        count: 0,
+        taskInput: null,
+        displaytext: null,
+        taskSection: false,
 
-    taskInput:null,
-    displaytext:null,
-    taskSection:false,
+        increment() {
+            this.count++;
+            console.log(this.count);
+        },
 
-    increment(){
-      this.count++
-      console.log(this.count);
-    },
-
-    addNote(){
-        console.log('tttt' + this.taskInput)
-        this.taskSection= true
-        this.displaytext = this.taskInput
-    }
-  }
-};
+        addNote() {
+            console.log('Note added: ' + this.taskInput);
+            this.taskSection = true;
+            this.displaytext = this.taskInput; // Display the note
+        }
+    };
+}
--- public/src/main.js
+++ public/src/main.js
@@ -2,6 +2,7 @@ console.log('Main.js is loaded!');
 import Alpine from 'alpinejs';
 import notesApp from '../notes-app.js'
 
+
 window.Alpine = Alpine;
 
 Alpine.data('notes',notesApp)
